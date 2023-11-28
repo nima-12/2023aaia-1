376226d1c844d49f36f3aea7e3a1846101be2e6a
nima-12 <ye6403@gmail.com>
2023-11-28 10:16:40 +0800

    week11

---
+++ week11/week11-3.py
@@ -0,0 +1,9 @@
+a=list(map(int,input().split()))
+
+print(sum(a)-a[0])
+
+
+
+
+a=list(map(int,input().split()))
+print(max(a))
---
+++ week11/week11-5.py
@@ -0,0 +1,12 @@
+a=int(input())
+
+if a-2>0:
+	print(a-2,end = '')
+else :
+	print(2-a.end='')
+
+
+a=input()
+print(a[0],a[1],a[2],a[3],a[4],sep='   ',end='')
+
+
